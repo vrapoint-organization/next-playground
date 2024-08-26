36a2b5456486c1e9466ddfd1b90ce0fcb75ea078
sni424 <jojonghyeok424@gmail.com>
2024-08-26 13:37:41 +0900

    feat: add i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+import i18n_pkg from "./next-i18next.config.js";
+
 const nextConfig = {
   reactStrictMode: true,
+  i18n: i18n_pkg.i18n,
 };
 
 export default nextConfig;
